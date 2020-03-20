9efdfead070f2931edacda0655f8c4a4e16683fd
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2020-03-20 08:11:08 +0000

    Add files via upload
    I can't get the geojson map to appear year, getting a 404 for some reason, but will work out why after work.  I was tired last night.

--- src/app/Components/ireland-map/ireland-map.component.ts
+++ src/app/Components/ireland-map/ireland-map.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import * as t from 'topojson';
+import { zoom } from 'd3';
 
 @Component({
   selector: 'app-ireland-map',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ireland-map.component.css']
 })
 export class IrelandMapComponent implements OnInit {
+  name = 'd3';
+  mapFeatures; any;
 
   constructor() { }
 
   ngOnInit(): void {
+
+    const width = 620;
+    const height = 396;
+    let data;
+
+    const projection = d3.geoMercator()
+      .translate([ width / 2, height / 2])
+      .scale(100);
+
+    const svg = d3.select('#ireland').append('svg')
+      .attr('width', width)
+      .attr('height', height);
+    const path = d3.geoPath()
+      .projection(projection);
+    const g = svg.append('g');
+    g.attr('class', 'map');
+
+    const counties = svg.append('svg:g')
+    .attr('id', 'ireland');
+    console.log(counties);
+
+    d3.json('ireland.json', json => {
+      counties.selectAll('path')
+        .data(json.features)
+        .enter().append('path')
+        .attr('class', 'ireland')
+        .attr('d', path);
+    });
 
   }
 
