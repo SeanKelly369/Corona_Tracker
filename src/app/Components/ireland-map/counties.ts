ee6adfd15694d48fad1d4ecabd7e822534aff419
Sean Kelly <36159246+SeanKelly369@users.noreply.github.com>
2020-03-21 23:29:15 +0000

    Add files via upload

---
+++ src/app/Components/ireland-map/counties.ts
@@ -0,0 +1,41 @@
+export class Counties {
+
+    counties = [
+        { id: 1, name : 'Cork'},
+        { id: 2, name : 'Galway'},
+        { id: 3, name : 'Donegal'},
+        { id: 4, name : 'South Dublin'},
+        { id: 5, name : 'Mayo'},
+        { id: 6, name : 'Waterford'},
+        { id: 7, name : 'Cork'},
+        { id: 8, name : 'Dublin North'},
+        { id: 9, name : 'Dún Laoghaire-Rathdown'},
+        { id: 10, name : null},
+        { id: 11, name : 'Galway'},
+        { id: 12, name : 'Waterford'},
+        { id: 13, name : 'Wexford'},
+        { id: 14, name : 'Louth'},
+        { id: 15, name : 'Limerick'},
+        { id: 16, name : 'Monaghan'},
+        { id: 17, name : 'South Tipperary'},
+        { id: 18, name : 'Fingal'},
+        { id: 19, name : 'Meath'},
+        { id: 20, name : 'Offaly'},
+        { id: 21, name : 'Westmeath'},
+        { id: 22, name : 'Wicklow'},
+        { id: 23, name : 'Kildare'},
+        { id: 24, name : 'Kilkenny'},
+        { id: 25, name : 'Laoighis'},
+        { id: 26, name : 'North Tipperary'},
+        { id: 27, name : 'Kerry'},
+        { id: 28, name : 'Limerick'},
+        { id: 29, name : 'Roscommon'},
+        { id: 30, name : 'Sligo'},
+        { id: 31, name : 'Leitrim'},
+        { id: 32, name : 'Longford'},
+        { id: 33, name : 'Clare'},
+        { id: 34, name : 'Carlow'},
+        { id: 35, name : 'Cavan'}
+
+    ]
+}
--- src/app/Components/ireland-map/ireland-map.component.ts
+++ src/app/Components/ireland-map/ireland-map.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
-import * as t from 'topojson';
-import { zoom } from 'd3';
+import { Counties } from './counties';
 
 @Component({
   selector: 'app-ireland-map',
@@ -9,41 +8,80 @@ import { zoom } from 'd3';
   styleUrls: ['./ireland-map.component.css']
 })
 export class IrelandMapComponent implements OnInit {
+
   name = 'd3';
-  mapFeatures; any;
+  mapFeatures: any;
+  airports: any;
+  regions = [];
 
-  constructor() { }
+  public ngOnInit() {
 
-  ngOnInit(): void {
-
-    const width = 620;
-    const height = 396;
-    let data;
+    const counties = new Counties().counties;
+    const width = 1020;
+    const height = 1500;
 
     const projection = d3.geoMercator()
+      .scale(12090)
+      .center([-9.6, 53.47])
       .translate([ width / 2, height / 2])
-      .scale(100);
 
     const svg = d3.select('#ireland').append('svg')
-      .attr('width', width)
+      .attr('width', 1600)
       .attr('height', height);
     const path = d3.geoPath()
       .projection(projection);
     const g = svg.append('g');
     g.attr('class', 'map');
 
-    const counties = svg.append('svg:g')
-    .attr('id', 'ireland');
-    console.log(counties);
+    // console.log(g);
 
-    d3.json('ireland.json', json => {
-      counties.selectAll('path')
-        .data(json.features)
-        .enter().append('path')
-        .attr('class', 'ireland')
-        .attr('d', path);
-    });
+    d3.json('../assets/maps/ireland_counties.geojson')
 
+      .then((topology: any) => {
+        // console.log(topology.features);
+
+        for ( let i = 0; i < 35; i++) {
+          this.regions.push(topology.features[i].properties.name);
+        }
+        console.log(this.regions);
+
+        g.selectAll('path')
+          .data(topology.features)
+          .enter()
+          .append('path')
+          .attr('d', path)
+          .attr('width', 500)
+          .attr('class', 'counties')
+          .attr('fill', '#218F5B')
+          .attr('stroke', '#A0FFD3')
+          .on('mouseover', function(d) {
+            // const mousePos = d3.mouse(this);
+            // const posX = Math.floor(mousePos[0]);
+            // const posY = Math.floor(mousePos[1]);
+            for ( let i = 0; i < 35; i++) {
+              if ( d.properties.name === topology.features[i].properties.name ) {
+                d3.select(this)
+                .style('fill', 'rgb(51, 196, 128)')
+                // .style('transform', `translate( ${posX + 8}px, ${posY + 10}px )`)
+                .style('background-color', '#33333388')
+                .style('padding', '4px')
+                .style('padding-left', '8px')
+                .style('padding-right', '8px')
+                .style('border-radius', '2px')
+                .style('display', 'block')
+                .text(d.properties.name);
+              }
+            }
+          })
+          .on('mouseout', d => {
+            for ( let i = 0; i < 35; i++) {
+              if ( d.properties.name !== topology.features[i].properties.name ) {
+                d3.selectAll('.counties')
+                .style('fill', '#218F5B')
+                .text('');
+              }
+            }
+          });
+        });
   }
-
 }
